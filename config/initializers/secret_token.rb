5cb1c1c1920ce363dfcea41f03a9997d80f46fc6
dash7board <dash7board.com>
2014-06-04 01:00:59 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '22e19cb9e94e3f1c33535dfa554c7ff7f21e3aacdd825377e247f8e43df314b2bac99b9d0a46413b1cbd7015ac7af25d47f48a360fd9ee56fe731dd87eee2d50'
